49fc3af5b1f9356f0750ea90e777a1907ef7deda
kimD0ngjun <chickentasty0112@gmail.com>
2025-06-29 01:27:15 +0900

    Null 가능성 변수에 대한 방어책으로 컴파일러 강제 납득시키기

--- src/main/kotlin/Nullable.kt
+++ src/main/kotlin/Nullable.kt
@@ -4,9 +4,16 @@ fun main() {
 
     // only int
     val inputInt = inputString.toInt()
-    print("your input is ${inputInt}") // it's possible to occur exception
+    print("your input is $inputInt") // it's possible to occur exception
 
     // int or null
     val inputValue = inputString.toIntOrNull()
-    print("your input is ${inputValue}") // prevent Null Exception
+    print("your input is $inputValue") // prevent Null Exception
+
+    // inputValue can be assigned Null, then this code is wrong
+//    val isEven = inputValue % 2 == 0
+
+    // Compiler will recognize 'isEven' might not be null
+    val isEven = inputValue!! % 2 == 0
+    print("your input is even : $isEven")
 }
