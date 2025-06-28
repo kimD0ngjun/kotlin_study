3465ed1a1b05b31a17acbcac136f7e1daee320c3
kimD0ngjun <chickentasty0112@gmail.com>
2025-06-29 01:44:55 +0900

    엘비스 연산자는 Null 안전한 연산자로 Null 발생시 세팅 디폴트 값으로 대체된다

--- src/main/kotlin/Nullable.kt
+++ src/main/kotlin/Nullable.kt
@@ -1,19 +1,58 @@
 fun main() {
+//    // input warning! NPE!
+//    problem()
+//
+//    // input is safe... but variable can be still null...
+//    preventAssignNull()
+//
+//    // compiler may know variable is not null... but it is still dangerous
+//    convinceCompiler()
+
+    // Elvis Operator is safe from NPE
+    getReplacement()
+}
+
+fun input(): String {
     print("input string : ")
-    val inputString = readln()
+    return readln()
+}
+
+fun problem() {
+    val inputString = input()
 
     // only int
     val inputInt = inputString.toInt()
     print("your input is $inputInt") // it's possible to occur exception
+}
+
+fun preventAssignNull() {
+    val inputString = input()
+
+    // int or null
+    val inputValue = inputString.toIntOrNull()
+    print("your input is $inputValue") // prevent Null Exception
+}
+
+fun convinceCompiler() {
+    val inputString = input()
 
     // int or null
     val inputValue = inputString.toIntOrNull()
     print("your input is $inputValue") // prevent Null Exception
 
-    // inputValue can be assigned Null, then this code is wrong
-//    val isEven = inputValue % 2 == 0
-
     // Compiler will recognize 'isEven' might not be null
     val isEven = inputValue!! % 2 == 0
     print("your input is even : $isEven")
+}
+
+fun getReplacement() {
+    val inputString = input()
+
+    // Elvis Operator
+    // if inputInt is null, inputInt is 0(default value)
+    val inputInt = inputString.toIntOrNull() ?: 0
+    println("your input is $inputInt")
+
+    val isEven = inputInt % 2 == 0
+    print("this is even : $isEven")
 }
