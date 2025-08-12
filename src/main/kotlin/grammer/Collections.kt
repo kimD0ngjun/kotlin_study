da9b7c85fe32af625ee4eebaade33e5791161a84
kimD0ngjun <chickentasty0112@gmail.com>
2025-08-13 00:51:23 +0900

    코틀린의 컬렉션: 리스트

---
+++ src/main/kotlin/grammer/Collections.kt
@@ -0,0 +1,39 @@
+package grammer
+
+fun main() {
+    /*
+    List
+     */
+
+    var readOnlyShape = listOf("tri", "rect", "cir")
+    println(readOnlyShape) // 자바의 읽기 전용 불변 리스트 메소드랑 똑같이 리스트 요소들 바로 출력되네
+//    readOnlyShape[0] = "new_tri"
+
+    var shapes: MutableList<String> = mutableListOf("tri", "rect", "cir")
+    shapes[0] = "new_tri"
+    println(shapes)
+
+    var noMutableList: List<String> = shapes
+//    noMutableList[0] = "new_new_tri"
+    // 다시 불변 컬렉션 회귀...
+    println(noMutableList)
+
+    // 문자열 템플릿도 가능
+    println("the first shape is ${noMutableList[0]}")
+    println("the first shape is ${noMutableList.first()}")
+//    println("the last shape is ${noMutableList[-1]}")
+    // 아쉽게도 파이썬처럼 구현되진 않았네잉... 코틀린의 컬렉션 사이즈는 .size
+    println("the last shape is ${noMutableList[noMutableList.size - 1]}")
+    println("the last shape is ${noMutableList.last()}")
+
+    // 카운팅, 존재 여부 확인도 가능
+    println("the count is ${noMutableList.count()}")
+    println("cir" in noMutableList)
+    println("penta" in noMutableList)
+
+    // 요소 추가 삭제도 가능
+    shapes.add("penta")
+    println("shapes: $shapes")
+    shapes.remove("penta")
+    println("shapes: $shapes")
+}
