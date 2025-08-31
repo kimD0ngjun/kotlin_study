2e3758945b4bcc0058a796945fca584f7dd49a28
kimD0ngjun <chickentasty0112@gmail.com>
2025-08-31 22:42:18 +0900

    확장 함수: 기존 클래스의 OCP와 캡슐화를 지키면서 함수 추가하기

---
+++ src/main/kotlin/intermediate/ExtensionFuction.kt
@@ -0,0 +1,41 @@
+package intermediate
+
+// 기존 자바에서는 기존 클래스에 함수를 추가하려면 상속이나 직접 OCP 위반하던지 등등...
+// 근데 그건 그렇고, 만약 외부 라이브러리 같은 경우면 더 추가하기 빡세짐
+// 코틀린은 그럴 필요 없이 '확장'이라는 개념 제시(애초에 코틀린은 원천적으로 클래스가 상속 불허)
+
+/**
+ * fun 확장할 클래스.함수명: 리턴타입 {
+ *    return 리턴값
+ * }
+ */
+
+// 단, 확장함수는 수신객체의 private이나 protected 멤버를 사용할수 없기 때문에, 해당 수신객체의 public 멤버에만 접근이 가능
+// 애시당초 외부에서 접근하는 구조이므로...
+
+// 문자열 클래스에 존재하지 않고, 내가 직접 커스텀한 볼드체 처리 메소드
+fun String.bold(): String = "<strong>$this</strong>"
+
+// 상당히 유연하면서도 캡슐화를 깨뜨리지 않기 때문에 매우 유용
+class HttpClient {
+    fun request(method: String, url: String, headers: Map<String, String>): HttpResponse {
+        println("Requesting $method to $url with headers: $headers")
+        return HttpResponse("Response from $url")
+    }
+}
+
+class HttpResponse(val response: String)
+
+fun HttpClient.get(url: String): HttpResponse = request("GET", url, emptyMap())
+
+fun main() {
+    println("hello".bold())
+
+    val client = HttpClient()
+
+    // Making a GET request using request() directly
+    val getResponseWithMember = client.request("GET", "https://example.com", emptyMap())
+
+    // Making a GET request using the get() extension function
+    val getResponseWithExtension = client.get("https://example.com")
+}
