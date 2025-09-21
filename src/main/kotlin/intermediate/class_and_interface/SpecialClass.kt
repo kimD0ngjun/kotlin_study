85d97da2116dd9f5c7bddbe369ff271277d24a70
kimD0ngjun <chickentasty0112@gmail.com>
2025-09-21 18:04:16 +0900

    코틀린의 특수 클래스 키워드 enum, 자바랑 완전 같음

--- src/main/kotlin/intermediate/class_and_interface/SpecialClass.kt
+++ src/main/kotlin/intermediate/class_and_interface/SpecialClass.kt
@@ -27,6 +27,30 @@ fun hello(mammal: Mammal): String {
     }
 }
 
+/**
+ * 자바에도 열거형 enum이 있듯, 코틀린에도 있음
+ * 단 자바에서 Enum과 Class가 다르게 취급된 것과 달리, 코틀린은 enum 키워드로 존재
+ * 동작 매커니즘은 똑같다. 심지어 자바 Enum으로 싱글톤 인스턴스 만드는 게 코틀린 enum class도 같음
+ * 얘 역시 인스턴스는 못 만듦, 하지만 생성자는 가진다
+ */
+enum class State(val state: String) {
+    // enum class가 로딩될 때 딱 한번 로딩되고
+    // 인스턴스가 이 시점에 열거형 필드 갯수(여기서는 3개)만큼 생김
+    IDLE("게으름"), RUNNING("뜀"), RIDING("탐");
+
+    fun introduce(): Unit = println("나는 현재 $state 상태다.")
+}
+
 fun main() {
     println(hello(Human("김동준", "백수")))
+    /**
+     * enum class의 생성자는 외부 호출용이 아닌 enum 상수 정의할 때 내부저으로 호출되는 용도
+     */
+//    val state = State("배고픈")
+    val idleState = State.IDLE
+    idleState.introduce() // 이미 클래스 로딩될 때 생성된 내부 인스턴스를 참조하는 것에 불과
+    val runningState = State.RUNNING
+    runningState.introduce() // 이미 클래스 로딩될 때 생성된 내부 인스턴스를 참조하는 것에 불과
+    val ridingState = State.RIDING
+    ridingState.introduce() // 이미 클래스 로딩될 때 생성된 내부 인스턴스를 참조하는 것에 불과
 }
