4421f1395b7279bd057a2db6d6638a18a6197af2
kimD0ngjun <chickentasty0112@gmail.com>
2025-10-09 17:28:56 +0900

    코루틴 빌더: CoroutineScope.asnyc() -> 자바의 Future<>와 비슷

--- src/main/kotlin/coroutines/basic/CoroutineBuilder.kt
+++ src/main/kotlin/coroutines/basic/CoroutineBuilder.kt
@@ -1,8 +1,12 @@
 package coroutines.basic
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.time.Duration.Companion.milliseconds
 
 /**
@@ -17,7 +21,12 @@ suspend fun main() {
      * 백그라운드 작업 실행
      */
 
-
+    asyncWork()
+    /**
+     * 두 번째 페이지
+     * 첫 번째 페이지
+     * 같은 페이지일까요? : false
+     */
 }
 
 /**
@@ -35,4 +44,30 @@ suspend fun performBackgroundWork() = coroutineScope {
 
     // 메인 코루틴은 백그라운드가 중단돼도 실행
     println("코루틴 스코프")
+}
+
+/**
+ * 2) CoroutineScope.async()
+ * CoroutineScope의 확장 함수
+ * 기존 코루틴 스코프 내에서 동시 연산 시 -> 최종 결과를 Deferred 타입 객체로 반환(자바 Future의 현대적 확장)
+ * 결과 준비를 위한 코드 일시중지는 await() 함수로
+ */
+suspend fun asyncWork() = withContext(Dispatchers.Default) {
+    // this: CoroutineScope
+    val firstPage: Deferred<String> = this.async {
+        delay(100.milliseconds)
+        println("첫 번째 페이지")
+        "첫 번째 페이지"
+    }
+
+    val secondPage: Deferred<String> = this.async {
+        delay(50.milliseconds)
+        println("두 번째 페이지")
+        "두 번째 페이지"
+    }
+
+    // 자바의 Future.get()처럼 await()도 결과를 갖고올 때까지 코드(블록 실행)를 멈춘다
+    // 다만 스레드 블로킹을 하지 않고 그냥 코루틴만 중단(suspend)한다
+    val pagesAreEqual: Boolean = firstPage.await() == secondPage.await()
+    println("같은 페이지일까요? : $pagesAreEqual")
 }
