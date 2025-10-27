c2b0ff7db8cc3034a0bfef278315d36a7a170705
kimD0ngjun <chickentasty0112@gmail.com>
2025-10-27 22:52:50 +0900

    코루틴 빌더: deferred 객체 설명

--- src/main/kotlin/coroutines/basic/CoroutineBuilderParameter.kt
+++ src/main/kotlin/coroutines/basic/CoroutineBuilderParameter.kt
@@ -1,5 +1,6 @@
 package coroutines.basic
 
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -7,14 +8,15 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 suspend fun main() {
-    perform()
+    launchJob()
+    asyncDeferred()
 }
 
 /**
  * 앞서 봤듯, 코루틴은 그 자체로 실행되지 않고 CoroutineScope(컨테이너) 내부에서 새로운 코루틴을 생성하는 함수가 필요하다
  * 그게 코루틴 빌더이며, 대표적으로 launch()와 async()가 있다.
  */
-suspend fun perform() = coroutineScope {
+suspend fun launchJob() = coroutineScope {
     // this: CoroutineScope
 
     // launch()는 결과값이 없는 코루틴을 실행한다.
@@ -31,10 +33,19 @@ suspend fun perform() = coroutineScope {
     println("job 상태 : ${job.isActive}")
     job.cancel() // 코루틴 실행 제어 -> 작업 취소!
     println("1초 지난 후 job 상태 : ${job.isActive} // 취소 여부 : ${job.isCancelled}")
+}
 
+/**
+ * await을 호출하면 해당 코루틴만 중단되고, 나머지는 그대로 동작한다
+ * 결과를 나중에 받아야 하지만, 기다리는 동안은 CPU를 놀리지 않게 한다
+ *
+ * -> 이게 future.get()과의 차이
+ */
+suspend fun asyncDeferred() = coroutineScope {
+    val sum1: Deferred<Int> = this.async { (1..100).sum() }
+    val sum2: Deferred<Int> = this.async { (101..200).sum() }
 
-    // async()는 결과값이 있는 코루틴을 실행한다.
-    this.async {
-
-    }
+    // Deferred 덕분에 await()으로 결과값 조합 가능
+    val total = sum1.await() + sum2.await()
+    println("총합: $total")
 }
